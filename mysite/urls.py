4367d9554024ab1d3ee2075b0c4a70b30c49e961
jennyyuejin <jin.yuejenny@gmail.com>
2013-08-26 22:04:25 -0700

    some chapter 8 -- cleaned up urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,28 +1,37 @@
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
-from mysite.views import hello, current_datetime, hours_ahead, display_meta
-from books import views as bookViews
-from contact import views as contactViews
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^hello/$', hello),
-    url(r'^time/$', current_datetime),
-    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
-    url(r'^meta/$', display_meta),
-    url(r'^search/$', bookViews.search),
-    url(r'^contact/$', contactViews.contact),
-    url(r'^contact/thanks/$', contactViews.thanks),
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^mysite/', include('mysite.foo.urls')),
+# mysite stuff
+urlpatterns = patterns('mysite.views',
+                       url(r'^hello/$', 'hello'),
+                       url(r'^time/$', 'current_datetime'),
+                       url(r'^time/plus/(?P<offset>\d{1,2})/$', 'hours_ahead'),)
+# books stuff
+urlpatterns += patterns('books.views',
+                        url(r'^search/$', 'search'),)
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+# contact stuff
+urlpatterns += patterns('contact.views',
+                        url(r'^contact/$', 'contact'),
+                        url(r'^contact/thanks/$', 'thanks'),)
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
+# default django stuff
+urlpatterns += patterns('',
+                       # Examples:
+                       # url(r'^$', 'mysite.views.home', name='home'),
+                       # url(r'^mysite/', include('mysite.foo.urls')),
 
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),)
+
+# debug stuff
+if settings.DEBUG:
+    urlpatterns += patterns('mysite.views',
+                           url(r'^meta/$', 'display_meta'),)
